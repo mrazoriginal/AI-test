01c85faefaa11ae439599269156ee8412663006f
Ali <mr.az.original@gmail.com>
2026-05-23 22:31:21 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-  document.getElementById("boot").style.display = "none";
-  document.getElementById("os").classList.remove("hidden");
-}, 1500);
-
-setInterval(() => {
-  const clock = document.getElementById("clock");
-  clock.textContent = new Date().toLocaleTimeString();
-}, 1000);
